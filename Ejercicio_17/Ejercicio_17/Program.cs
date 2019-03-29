af1b05dbbab1fcc6f1111b2a54537c2d2f28d2ea
413831 <alepleins3pp@gmail.com>
2019-03-28 22:01:18 -0300

    Clase 04 28/03/19

---
+++ Ejercicio_17/Ejercicio_17/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_17
+{
+    class Boligrafo
+    {
+        const short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+
+        public Boligrafo(short tinta, ConsoleColor color)
+        {
+
+        }
+
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+
+        public bool Pintar(int gasto,out string dibujo)
+        {
+            if(gasto < this.tinta)
+            {
+                 this.SetTinta((short)-gasto);
+
+            
+                String.dibujo = 
+            }
+            dibujo = "dibujo";
+            return true;
+        }
+        public void Recargar()
+        {
+            this.SetTinta(cantidadTintaMaxima);
+        }
+        private void SetTinta(short tinta)
+        {
+            if((this.tinta + tinta) < cantidadTintaMaxima || (this.tinta + tinta) > 0)
+            {
+                this.tinta = tinta;
+            }   
+        }
+
+
+        static void Main(string[] args)
+        {
+            Boligrafo boligrafoAzul = new Boligrafo(100, ConsoleColor.Blue);
+            Boligrafo boligrafoRojo = new Boligrafo(50, ConsoleColor.Red);
+
+        }
+    }
+}
