aeb2502c937ece3d45e6b275a4aa18310e55ba7a
alumno <alepleins3pp@gmail.com>
2019-04-17 22:02:30 -0300

    Ejercicio 28

---
+++ Ejercicio_28/Ejercicio_28/Diccionario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ *Utilizar Diccionarios (Dictionary<string, int>) para realizar un contador de palabras, recorriendo el
+    texto palabra por palabra se deberá lograr que si se trata de una nueva palabra, se la agregará al
+    diccionario e inicializar su contador en 1, caso contrario se deberá incrementar dicho contador.
+    Ordenar los resultados de forma descendente por cantidad de apariciones de cada palabra.
+    Informar mediante un MessageBox el TOP 3 de palabras con más apariciones. 
+ */
+namespace Ejercicio_28
+{
+    public class Diccionario
+    {
+        public static Dictionary<string, int> diccionario;
+
+        static Diccionario()
+        {
+            diccionario = new Dictionary<string, int>();
+        }
+
+        public static void SetValue(string palabra)
+        {
+            if(diccionario.ContainsKey(palabra))
+            {
+                diccionario[palabra] += 1; 
+            }
+            else
+            {
+               diccionario.Add(palabra,1);
+            }
+        }
+    }
+}
---
+++ Ejercicio_28/Ejercicio_28/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String textValues = "";
+            KeyValuePair<string, int> auxiliar;
+
+            string[] texto;
+
+            texto = lblTexto.Text.Split(' ');
+
+            foreach(string palabra in texto)
+            {
+                Diccionario.SetValue(palabra);
+            }
+
+            for (int i=0; i < 3;i++)
+            {
+                auxiliar = Diccionario.diccionario.Max();
+                Diccionario.diccionario.Remove(auxiliar.Key);
+                textValues += "\n"+auxiliar.ToString();
+            }
+            MessageBox.Show(textValues);
+        }
+
+    }
+}
