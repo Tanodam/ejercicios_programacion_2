8a24b5b23a9a32f2cae43a74efe6cdcbd93b6c7e
413831 <alepleins3pp@gmail.com>
2019-04-19 17:23:13 -0300

    Ejercicio 31

---
+++ Ejercicio_31/Ejercicio_31/Negocio.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_31
+{
+    public class Negocio
+    {
+        private PuestoAtencion caja;
+        private Queue<Cliente> clientes;
+        private string nombre;
+
+        #region Propiedades
+
+        public Cliente Cliente
+        {
+            get
+            {
+                return clientes.Dequeue();
+            }
+            set
+            {
+                if(!clientes.Contains(value))
+                {
+                    clientes.Enqueue(value);
+                }
+            }
+        }
+
+        public byte ClientesPendientes
+        {
+            get
+            {
+                return (byte)this.clientes.Count;
+            }
+        }
+
+        #endregion
+
+        #region Constructores
+
+        private Negocio()
+        {
+            this.clientes = new Queue<Cliente>();
+            this.caja = new PuestoAtencion(Puesto.Caja1);
+        }
+
+        public Negocio(string nombre) : this()
+        {
+            this.nombre = nombre;
+        }
+
+        #endregion
+
+        #region Operadores
+
+        public static bool operator !=(Negocio negocio, Cliente cliente)
+        {
+            return !(negocio == cliente);
+        }
+
+        public static bool operator ==(Negocio negocio, Cliente cliente)
+        {
+            if(negocio.clientes.Contains(cliente))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator ~(Negocio negocio)
+        {
+            if(negocio.caja.Atender(negocio.Cliente))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator +(Negocio negocio, Cliente cliente)
+        {
+            foreach(Cliente clienteAux in negocio.clientes)
+            {
+                if(clienteAux == cliente)
+                {
+                    return false;
+                }
+            }
+            negocio.clientes.Enqueue(cliente);
+            return true;
+        }
+
+        //
+        //OVERRIDE EQUALS
+        //
+        public override bool Equals(object obj)
+        {
+            if (obj != null || this.GetType() != obj.GetType())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Tuple.Create(caja, clientes, nombre).GetHashCode();
+        }
+        #endregion
+    }
+}
---
+++ Ejercicio_31/Ejercicio_31/PuestoAtencion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Ejercicio_31
+{ 
+    public enum Puesto
+    {
+        Caja1,
+        Caja2
+    }
+
+    public class PuestoAtencion
+    {
+        private static int numeroActual;
+        private Puesto puesto;
+
+        #region Propiedades
+
+        public static int NumeroActual
+        {
+            get
+            {
+                numeroActual++;
+                return numeroActual;
+            }
+        }
+
+        #endregion
+
+        private PuestoAtencion()
+        {
+            numeroActual = 0;
+        }
+
+        public PuestoAtencion(Puesto puesto)
+        {
+
+        }
+
+        public bool Atender(Cliente cliente)
+        {
+            if(cliente != null)
+            {
+                Console.Write("Atendiendo al cliente...");
+                Thread.Sleep(3000);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+    }
+}
