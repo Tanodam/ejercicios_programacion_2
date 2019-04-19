ab1c1a5d67ed51e8a4ddff1143e881a4b771a735
413831 <alepleins3pp@gmail.com>
2019-04-19 18:54:28 -0300

    Ejercicio 33 para corregir

---
+++ Ejercicio_33/Ejercicio_33/Libro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_33
+{
+    public class Libro
+    {
+        private List<string> paginas;
+
+        public Libro()
+        {
+            this.paginas = new List<string>();
+        }
+
+        public string this[int i]
+        {
+            get
+            {
+                if(i < paginas.Count)
+                {
+                    return paginas[i];
+                }
+                else
+                {
+                    return "";
+                }
+
+            }
+            set
+            {
+                if(i == paginas.Count)
+                {
+                    paginas[i] = value;
+                }
+            }
+
+        }
+    }
+}
---
+++ Ejercicio_33/Ejercicio_33/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro libro = new Libro();
+
+            libro[0] = "Pagina Uno";
+            libro[1] = "Pagina Dos";
+            libro[2] = "Pagina Tres";
+            libro[3] = "Pagina Cuatro";
+            libro[4] = "Pagina Cinco";
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write(libro[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
